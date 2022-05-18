8f6efa05399f5f091bf86796dafe33d375f74b44
Filip <filipjanigaa@gmail.com>
2022-05-18 20:07:14 +0200

    crud for categories

---
+++ tests/Controller/TaskControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Task controller tests.
+ */
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class TaskControllerTest.
+ */
+class TaskControllerTest extends WebTestCase
+{
+    /**
+     * Test route.
+     *
+     * @const string
+     */
+    public const TEST_ROUTE = '/task';
+
+    /**
+     * Test client.
+     */
+    private KernelBrowser $httpClient;
+
+    /**
+     * Set up tests.
+     */
+    public function setUp(): void
+    {
+        $this->httpClient = static::createClient();
+    }
+
+    /**
+     * Test route.
+     */
+    public function testRoute(): void
+    {
+        // given
+        $expectedStatusCode = 200;
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+}
